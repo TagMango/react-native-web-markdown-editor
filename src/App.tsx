548a00bf004ab86d1026e9134ba843d8abfbeda0
KylixMedusa <ayush.agarwalmng@gmail.com>
2022-01-05 13:07:18 +0530

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { convertToRaw, EditorState } from "draft-js";
+// @ts-ignore
+import draftToMarkdown from "draftjs-to-markdown";
+import anchorme from "anchorme";
+import "./App.css";
 
-function App() {
+declare global {
+  interface Window {
+    ReactNativeWebView: any;
+  }
+}
+
+const toolbarMango = {
+  options: ["inline", "blockType"],
+  inline: {
+    inDropdown: false,
+    className: "inlineTool",
+    component: undefined,
+    dropdownClassName: "toolbarDropdown",
+    options: ["bold", "italic", "underline"],
+  },
+  blockType: {
+    inDropdown: true,
+    options: ["Normal", "H1", "H2", "H3"],
+    className: "blockTool",
+    component: undefined,
+    dropdownClassName: "toolbarDropdown",
+  },
+};
+const hashConfig = {
+  trigger: "#",
+  separator: " ",
+};
+const config = {
+  blockTypesMapping: {
+    "unordered-list-item": "* ",
+  },
+  emptyLineBeforeBlock: true,
+};
+
+const App: React.FC = () => {
+  const [aboutYou, setAboutYou] = useState("");
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const onEditorStateChange = (state: EditorState) => {
+    setEditorState(state);
+  };
+  const onContentStateChange = () => {
+    const rawContentState = convertToRaw(editorState.getCurrentContent());
+    const markup = draftToMarkdown(rawContentState, hashConfig, config);
+    if (markup) {
+      setAboutYou(anchorme(markup));
+    }
+  };
+  useEffect(() => {
+    window.ReactNativeWebView?.postMessage(aboutYou);
+  }, [aboutYou]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor
+        editorState={editorState}
+        toolbar={toolbarMango}
+        placeholder="Add description"
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="emailBodyEditorWrapper"
+        editorClassName="editorClassName"
+        onEditorStateChange={onEditorStateChange}
+        onContentStateChange={onContentStateChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
